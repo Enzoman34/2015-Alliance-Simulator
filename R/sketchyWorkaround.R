f104f1f366cc850a9a9055fadabcd94ba39b2a51
Quinn Schiller <quinn@Quinns-MacBook-Air-2.local>
2015-01-08 18:21:37 -0800

    Changed slightly

--- R/sketchyWorkaround.R
+++ R/sketchyWorkaround.R
@@ -4,11 +4,13 @@ library(rPython)
 python.load("/Users/quinn/Documents/2015-Alliance-Simulator/2015\ Full\ Alliance.py")
 
 # Get the variable
-names <- python.get("col1")
-times <- as.numeric(python.get("col2"))
-points <- as.numeric(python.get("col3"))
+# names <- python.get("col1")
+# times <- as.numeric(python.get("col2"))
+# points <- as.numeric(python.get("col3"))
+test <-as.numeric(python.get(resultTable))
 
 plot(times,points)
 
-Allthedata <- as.data.frame(matrix(c(names,times,points),ncol=3,dimnames=as.list(c("Names","Time","Points"))))
-write.table(Allthedata,quote=F)
+Allthedata <- as.data.frame(matrix(c(names,times,points),ncol=3))
+names(Allthedata)<- c("Robot","Time","Points")
+write.csv(Allthedata,quote=F,row.names=F,file="checkthisout.csv")
